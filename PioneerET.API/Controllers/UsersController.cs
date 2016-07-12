eec3d7295685a317ac9bfb8c8f332a04e4211f8f
Burhan Farooq <burhan.farooq.ws@gmail.com>
2016-07-12 22:12:44 +0500

    Database connectivity layer

---
+++ PioneerET.API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using PioneerET.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PioneerET.API.Controllers
+{
+    public class UsersController : ApiController
+    {
+        /// <summary>
+        /// Gets All Users listed
+        /// </summary>
+        /// <returns>List of Users</returns>
+        public IHttpActionResult Get()
+        {
+            return Ok(new Users().Get());
+        }
+    }
+}
---
+++ PioneerET.Data/Users.cs
@@ -0,0 +1,31 @@
+﻿using PioneerET.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PioneerET.Data
+{
+    public class Users
+    {
+        public List<User> Get()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["PioneerETConnectionString"].ConnectionString;
+            using (var db = new PioneerETDataContext(cs))
+            {
+                return (from u in db.PioneerET_Users_from_LDAP
+                        orderby u.FirstName
+                        select new User()
+                        {
+                            FirstName = u.FirstName,
+                            LastName = u.LastName,
+                            EmailAddress = u.EmailAddress,
+                            PioneerUserIdentifier = u.PioneerUserIdentifier,
+                            Role = u.MemberOfGroups
+                        }).ToList();
+            }
+        }
+    }
+}
